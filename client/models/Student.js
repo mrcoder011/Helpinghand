e2604e907789449f637177308d740a1d10cab34f
MD Shafatullah <yourgithubemail@gmail.com>
2026-03-08 16:13:33 +0530

    Local changes before pull

--- client/models/Student.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const studentSchema = new mongoose.Schema({
-  email: String,
-  password: String
-});
-
-module.exports = mongoose.model("Student", studentSchema);
